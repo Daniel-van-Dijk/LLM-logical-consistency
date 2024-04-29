5be6cfc27642cdfdedda298fd448668f38a923ff
Daniel van Dijk <danielgerald@live.nl>
2024-04-29 16:35:03 +0200

    remove prefix from env

--- src/models/mistral7B.py
+++ src/models/mistral7B.py
@@ -1,9 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from huggingface_hub import login
+
 # make token on hugging face and insert here
 huggingface_token = "insert_token"
 login(token=huggingface_token)
-
 
 device = "cuda" 
 
@@ -11,9 +11,9 @@ model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
 messages = [
-    {"role": "user", "content": "Matthew is excellent and experienced.	Matthew is excellent. entailment, contradiction or neutral?"},
-    {"role": "assistant", "content": "entailment"},
-    {"role": "user", "content": "Dave is fitter than Andrea. Andrea is fitter than Dave. entailment, contradiction or neutral?"}
+    {"role": "user", "content": "Matthew is excellent and experienced.	Matthew is excellent. a: entailment, b: contradiction or c: neutral?"},
+    {"role": "assistant", "content": "a"},
+    {"role": "user", "content": "Dave is fitter than Andrea. Andrea is fitter than Dave. a: entailment, b: contradiction or c: neutral?"}
 ]
 
 encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
@@ -21,6 +21,6 @@ encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
 model_inputs = encodeds.to(device)
 model.to(device)
 
-generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
+generated_ids = model.generate(model_inputs, max_new_tokens=1, do_sample=True)
 decoded = tokenizer.batch_decode(generated_ids)
 print(decoded[0])
