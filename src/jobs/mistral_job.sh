cfc04bff36f03e5e1138b0162c803b30f6aa3043
vasiliskarlis <vasilis.karlis@student.uva.nl>
2024-04-30 14:03:23 +0300

    mistral job and global env

---
+++ src/jobs/mistral_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+  
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=9
+#SBATCH --gpus=1
+#SBATCH --job-name=mistral
+#SBATCH --ntasks=1
+#SBATCH --time=04:00:00
+#SBATCH --mem=32000M
+#SBATCH --output=output/mistral_%A.out
+
+
+
+# Run your code
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate nlp_package
+srun python -u models/mistral7B.py
