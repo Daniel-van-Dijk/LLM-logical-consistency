4d36c2ec8819cef96247c8ebf6b5f66913369066
vasiliskarlis <vasilis.karlis@student.uva.nl>
2024-05-08 15:42:41 +0300

    Zero shot chain of thought implementation

--- src/main.py
+++ src/main.py
@@ -15,11 +15,13 @@ def get_args_parser():
     parser.add_argument('--prompt-template', default='supported', 
                         choices=['entailment', 'truth', 'supported', 'logically_follow', 'multiple_choice'], 
                         help='choose prompt template')
+    parser.add_argument('--prompt_type',default='zero_shot_cot',choices=[ 'zero_shot', 'zero_shot_cot', 'few_shot','few_shot_cot'],
+                         help= 'choose prompt type' )
     return parser
 
 
 
-def run_tasks(tasks, model_name, prompt_style):
+def run_tasks(tasks, model_name, prompt_style, prompt_type):
     # TODO: design prompt with CoT
     # TODO: zero-shot vs few-shot
     # TODO: improve templates based on answer variations of model
@@ -45,12 +47,27 @@ def run_tasks(tasks, model_name, prompt_style):
         processed_data = process_tsv(file_path)
         answers = []
         for entry in processed_data:  
-            instruction = f'Premise: "{entry[1]}" Hypothesis: "{entry[2]}" {instruction_format}'
-            print("Prompt: ", instruction)
-            prompt = [{"role": "user", "content": instruction}]
-            output = model.inference_for_prompt(prompt=prompt)
-            print(f"Model output: {output}")
-            
+            if prompt_type == 'zero_shot_cot':
+                instruction_1 = f'Q: Premise- "{entry[1]}" Hypothesis- "{entry[2]}" {instruction_format}? A: Lets think step by step '
+                print("Prompt instruction 1: ", instruction_1)
+                message_1 = [{"role": "user", "content": instruction_1}]
+                output_1 = model.inference_for_prompt(prompt=message_1)
+                instruction_2= instruction_1+ output_1 + 'Therefore the answer among entailment, contradiction and neutral is: '
+                print("-------------------------------------------------")
+                print("Prompt instruction 2: ", instruction_2)
+                message_2 = [{"role": "user", "content": instruction_2}]
+                output = model.inference_for_prompt(prompt=message_2)
+                print("------------------------------------------------")
+                print(f"Model output: {output}")
+
+
+            else:
+                instruction = f'Premise: "{entry[1]}" Hypothesis: "{entry[2]}" {instruction_format}'
+                print("Prompt: ", instruction)
+                prompt = [{"role": "user", "content": instruction}]
+                output = model.inference_for_prompt(prompt=prompt)
+                print(f"Model output: {output}")
+                
             answers.append((instruction, output, entry[0]))
         
         if prompt_style in ['entailment', 'truth',  'supported', 'logically_follow']:
@@ -74,5 +91,5 @@ if __name__ == "__main__":
     args = get_args_parser()
     args = args.parse_args()
     print(f'Model: {args.model}')
-    average_accuracy = run_tasks(args.task, args.model, args.prompt_template)
+    average_accuracy = run_tasks(args.task, args.model, args.prompt_template, args.prompt_type)
     print('average accuracy: ', average_accuracy)
