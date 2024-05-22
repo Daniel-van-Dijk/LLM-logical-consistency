3971845e31806599603f408a19b44cd0c3ec8d4a
Daniel van Dijk <danielgerald@live.nl>
2024-05-22 18:15:05 +0200

    add batching to collecting data

--- src/collect_finetune_data.py
+++ src/collect_finetune_data.py
@@ -12,11 +12,11 @@ import time
 
 from preprocess import *
 from utils import *
-from prompters import create_prompter_from_str, CollectDataPrompter
+from prompters import *
 
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('LoNLI evaluation with LLMs', add_help=False)
+    parser = argparse.ArgumentParser('LoNLI finetune data collection', add_help=False)
     
     parser.add_argument('--model', default='mistral7B', type=str, metavar='MODEL',
                         help='model to run inference on')
@@ -29,26 +29,38 @@ def get_args_parser():
     parser.add_argument('--prompt-template', default='supported', type=str,
                         choices=['mcq'], 
                         help='choose prompt template')
-    parser.add_argument('--prompt-type', default='zero_shot_collect', type=str,
-                        choices=['zero_shot_collect'],
+    parser.add_argument('--prompt-type', default='zero_shot', type=str,
+                        choices=['zero_shot'],
                         help='choose prompt type')
     parser.add_argument('--evaluation_type', default='None', type=str, choices=['None'])
     parser.add_argument('--output_dir', default='finetune_data', type=str, metavar='OUTPUT_DIR',
                         help='dir to store data')
+    parser.add_argument('--batch_size', default=24, type=int, metavar='BATCH_SIZE',
+                        help='batch size for inference')
     return parser
 
 
-def run_tasks(tasks: List[str], model_name: str, output_dir: str):
+def run_tasks(tasks: List[str], model_name: str, prompt_type: str, batch_size: int, output_dir: str) -> List:
     start_time = time.time()
     general_instruction = 'Please read the multiple-choice question below carefully and select ONE of the listed options and only give a single letter.'
     prompt_templates = {
-        'mcq1': 'Given the premise provided, is the hypothesis: A. entailment or B. neutral or C. contradiction ? \n Answer: ',
-        'mcq2': 'Given the premise provided, is the hypothesis: A. entailment or B. contradiction or C. neutral ? \n Answer: ',
-        'mcq3': 'Given the premise provided, is the hypothesis: A. neutral or B. entailment or C. contradiction ? \n Answer: ',
-        'mcq4': 'Given the premise provided, is the hypothesis: A. neutral or B. contradiction or C. entailment ? \n Answer: ',
-        'mcq5': 'Given the premise provided, is the hypothesis: A. contradiction or B. neutral or C. entailment ? \n Answer: ',
-        'mcq6': 'Given the premise provided, is the hypothesis: A. contradiction or B. entailment or C. neutral ? \n Answer: '
+        'mcq1': 'Given the premise provided, is the hypothesis: A. entailment or B. neutral or C. contradiction ? \n Answer:',
+        'mcq2': 'Given the premise provided, is the hypothesis: A. entailment or B. contradiction or C. neutral ? \n Answer:',
+        'mcq3': 'Given the premise provided, is the hypothesis: A. neutral or B. entailment or C. contradiction ? \n Answer:',
+        'mcq4': 'Given the premise provided, is the hypothesis: A. neutral or B. contradiction or C. entailment ? \n Answer:',
+        'mcq5': 'Given the premise provided, is the hypothesis: A. contradiction or B. neutral or C. entailment ? \n Answer:',
+        'mcq6': 'Given the premise provided, is the hypothesis: A. contradiction or B. entailment or C. neutral ? \n Answer:'
     }
+    # keep correspondence of labels since we shuffle answer options 
+    label_mappings = {
+        'mcq1': {'A': 'entailment', 'B': 'neutral', 'C': 'contradiction'},
+        'mcq2': {'A': 'entailment', 'B': 'contradiction', 'C': 'neutral'},
+        'mcq3': {'A': 'neutral', 'B': 'entailment', 'C': 'contradiction'},
+        'mcq4': {'A': 'neutral', 'B': 'contradiction', 'C': 'entailment'},
+        'mcq5': {'A': 'contradiction', 'B': 'neutral', 'C': 'entailment'},
+        'mcq6': {'A': 'contradiction', 'B': 'entailment', 'C': 'neutral'}
+    }
+    prompter: DefaultPrompter = create_prompter_from_str(prompt_type)
 
     if model_name == 'hermes13B':
         model = Hermes13B()
@@ -58,58 +70,50 @@ def run_tasks(tasks: List[str], model_name: str, output_dir: str):
         model = LLama3_8B()
     elif model_name == 'starling7B':
         model = Starling7B()
+        prompter = StarlingZeroShot()
 
-    collect_prompter = CollectDataPrompter()
-
-    """
-    Logical tasks are: comparative, spatial, temporal, quantifier, numerical
-    So if we take 100 per task (50 from 2 separate files), we have 500 samples per model.
-    """
-
+    os.makedirs(output_dir, exist_ok=True)
     results = []
-
     for task in tasks:
         print('\n\n\n')
         print('==========================================')
-        print(f'Collecting 50 responses for task: {task}' )
+        print(f'Collecting predictions for task: {task}')
         file_path = f'data/{task}.tsv'
         processed_data = process_tsv(file_path)
-        for num, entry in enumerate(processed_data[:50]):
-            # pick random shuffle of answer options to avoid selection bias
+
+        batched_prompts, batched_mappings, batched_labels = [], [], []
+        num_processed = 0
+        for entry in processed_data[:50]:
+            # Pick random shuffle of answer options to avoid selection bias and store corresponding labels
             random_template_key = random.choice(list(prompt_templates.keys()))
             instruction_format = prompt_templates[random_template_key]
+            label_mapping = label_mappings[random_template_key]
 
-            instruction = collect_prompter.create_instruction(
+            instruction = prompter.create_instruction(
                 general_instruction=general_instruction,
-                premise=entry[1], 
-                hypothesis=entry[2], 
+                premise=entry[1],
+                hypothesis=entry[2],
                 instruction_format=instruction_format
             )
-            
-            print(f'Question number: {num}')
-            print("Prompt: ", instruction)
-            output = model.inference_for_prompt(prompt=instruction)
-            print(f"Model output: {output}")
-            print('==========================================')
-            print('\n')
-            
-            question_asked: str = instruction[-1]["content"]
-            result_entry = {
-            "task": task,
-            "question_number": num,
-            "question": question_asked,
-            "answer": output,
-            "question_and_answer": f"{question_asked}\n\n{output}",
-            "instruction_and_answer": f"{instruction_format}\n\n{output}"
-            }
-            results.append(result_entry)
-    
-    os.makedirs(output_dir, exist_ok=True)
-    output_file_path = os.path.join(output_dir, f'{model_name}_finetune_data.json')
-    with open(output_file_path, 'w') as json_file:
-        json.dump(results, json_file, indent=4)
+            batched_prompts.append(instruction)
+            batched_mappings.append(label_mapping)
+            batched_labels.append(entry[0])
 
-    print(f"Results saved to {output_file_path}")
+            # Fill batch with randomly shuffled prompts
+            if len(batched_prompts) == batch_size:
+                results, num_processed = process_batch(model, batched_prompts, batched_mappings, batched_labels, task, num_processed, results)
+                # empty batch again
+                batched_prompts, batched_mappings = [], []
+
+        # process potential remaining samples which did not fit in the final batch
+        if batched_prompts:
+            results, num_processed = process_batch(model, batched_prompts, batched_mappings, batched_labels, task, num_processed, results)
+
+        print(len(results))
+
+    output_path = f'{output_dir}/{model_name}_{prompt_type}_finetune.json'
+    with open(output_path, 'w') as f:
+        json.dump(results, f, indent=4)
     end_time = time.time()
     elapsed_time = (end_time - start_time) / 60
     print(f"Total time taken: {elapsed_time} minutes")
@@ -119,7 +123,14 @@ def run_tasks(tasks: List[str], model_name: str, output_dir: str):
 if __name__ == "__main__":
     args = get_args_parser()
     args = args.parse_args()
-    validate_args(args)
-    print(f'Collecting responses for model: {args.model}')
-    average_accuracy = run_tasks(args.task, args.model, args.output_dir)
-    print('Average accuracy: ', average_accuracy)
+    print(args)
+    print(f'Model: {args.model}')
+
+    print("Task list: ", args.task)
+    average_accuracy = run_tasks(
+        args.task, 
+        args.model, 
+        args.prompt_type,
+        args.batch_size,
+        args.output_dir
+    )
